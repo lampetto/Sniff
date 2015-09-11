ba505b33a85d628ffd1320511d0ee2f89cb90ce8
Gianloko <gangigia@gmail.com>
2015-09-11 13:09:56 +0200

    first commit Gian

---
+++ src/main/java/com/sniff/config/AppConfig.java
@@ -0,0 +1,9 @@
+package com.sniff.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.sniff")
+class AppConfig {
+}
